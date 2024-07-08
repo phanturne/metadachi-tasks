0bf6766f24e38f7694221e982f6e436be9210865
Kevin Ding <kding60@gatech.edu>
2024-07-07 23:17:08 -0700

    Adding constants.ts and limits.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,12 @@
 export const Routes = {
 	Home: "/",
+	Login: "/login",
+	SignUp: "/sign-up",
+	Setup: "/setup",
+	ResetPassword: "reset-password",
+	Assistants: "assistants",
+	Settings: "/settings",
+	Profile: "/profile",
+	Help: "/help",
+	NotFound: "/404",
 };
---
+++ lib/db/limits.ts
@@ -0,0 +1,8 @@
+// Adapted from https://github.com/mckaywrigley/chatbot-ui/tree/main/db
+
+// Profiles
+export const PROFILE_BIO_MAX = 500;
+export const PROFILE_DISPLAY_NAME_MAX = 100;
+export const PROFILE_CONTEXT_MAX = 1500;
+export const PROFILE_USERNAME_MIN = 3;
+export const PROFILE_USERNAME_MAX = 25;
